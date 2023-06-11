90f3e8fc6222adcfe3fa1259081ba853b2a945d4
Ahmadraza965 <120107650+Ahmadraza965@users.noreply.github.com>
2023-06-12 01:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+ document.getElementById("urlForm").addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      // Get input values
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+
+      // Generate query string
+      var queryString = "name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+      // Update URL text
+      document.getElementById("url").textContent = "https://localhost:8080/?" + queryString;
+    });
